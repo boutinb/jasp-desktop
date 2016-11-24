6528c08d8765ef8a01773b184fc1941cb3c923da
vankesteren <erikjan.vankesteren@gmail.com>
2016-11-24 18:16:32 +0100

    Edit hack solution

--- JASP-Engine/JASP/R/common.R
+++ JASP-Engine/JASP/R/common.R
@@ -1005,7 +1005,8 @@ saveImage <- function(plotName, format, width, height){
 	# Hacky solution to problem with last element being NULL
 	if (class(state[["figures"]][[plotName]]) == "recordedplot"){
 				print("HACK")
-				plt <- state[["figures"]][[plotName]][-length(state[["figures"]][[plotName]])]
+				#[-length(state[["figures"]][[plotName]])]
+				plt <- state[["figures"]][[plotName]]
 				class(plt) <- "recordedplot"
 	} else {
 		plt <- state[["figures"]][[plotName]]
