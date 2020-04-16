c38e7cbcb307636e1a95cc9b45cd8f560fff86e7
bruno boutin <b.boutin@uva.nl>
2020-04-16 17:43:46 +0200

    Add MarginMeans contrasts widget

---
+++ JASP-Desktop/components/JASP/Widgets/MarginalMeansContrastsTableView.qml
@@ -0,0 +1,45 @@
+//
+// Copyright (C) 2013-2018 University of Amsterdam
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with this program.  If not, see
+// <http://www.gnu.org/licenses/>.
+//
+
+
+import QtQuick 2.0
+import QtQuick.Layouts 1.11
+import JASP.Controls 1.0
+
+BasicThreeButtonTableView
+{
+	id				: customContrastsTV
+
+	itemType		: "double"
+	modelType		: "MarginalMeansContrasts"
+	cornerText		: ""
+	minimum			: -Infinity
+	decimals		: 3
+
+
+	buttonAddText		: qsTr("Add Contrast")
+	onAddClicked		: tableView.addRow()
+
+	buttonDeleteText	: qsTr("Delete Contrast")
+	onDeleteClicked		: tableView.removeARow()
+	buttonDeleteEnabled	: tableView.rowCount > 1
+
+	buttonResetText		: qsTr("Reset")
+	onResetClicked		: tableView.reset()
+	buttonResetEnabled	: tableView.rowCount > 0
+}
---
+++ JASP-Desktop/widgets/listmodelmarginalmeanscontrasts.h
@@ -0,0 +1,61 @@
+//
+// Copyright (C) 2013-2019 University of Amsterdam
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with this program.  If not, see
+// <http://www.gnu.org/licenses/>.
+//
+
+#ifndef LISTMODELMARGINALMEANSCONTRASTS_H
+#define LISTMODELMARGINALMEANSCONTRASTS_H
+
+#include "listmodeltableviewbase.h"
+
+class ListModelMarginalMeansContrasts : public ListModelTableViewBase
+{
+	Q_OBJECT
+	Q_PROPERTY(QString		colName		READ colName	WRITE setColName	NOTIFY colNameChanged	)
+
+public:
+	explicit ListModelMarginalMeansContrasts(BoundQMLTableView * parent);
+
+	int				getMaximumColumnWidthInCharacters(size_t columnIndex)	const	override;
+	void			loadColumnInfo();
+	void			modelChangedSlot()												override;
+	OptionsTable *	createOption()													override;
+	void			initValues(OptionsTable * bindHere)								override;
+	void			reset()															override;
+
+	QString			colName()												const				{ return _colName;	}
+	QString			getDefaultColName(size_t index)								const	override;
+	QString			getDefaultRowName(size_t index)								const	override	{ return tr("Contrast %1").arg(index + 1); }
+
+public slots:
+	void setColName(QString colName);
+	void dataSetChangedHandler();
+	void factorsSourceChanged();
+
+signals:
+	void colNameChanged(QString colName);
+
+private:
+	void modifyValuesNamesEtcetera();
+	void setFactors();
+	void setColLabels();
+
+private:
+	QString								_colName;
+	QMap<QString, QList<QString> >		_factors;
+};
+
+#endif // LISTMODELMARGINALMEANSCONTRASTS_H
